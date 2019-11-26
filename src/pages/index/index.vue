b8730af0d27043cf11c79577f69edf317d21b89f
Jessica <529933143@qq.com>
2019-11-26 16:29:09 +0800

    update kk1

---
+++ src/pages/index/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <i-grid>
+    <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/burger.png" />
+        </i-grid-icon>
+        <i-grid-label>汉堡</i-grid-label>
+    </i-grid-item>
+    <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/chicken.png" />
+        </i-grid-icon>
+        <i-grid-label>炸鸡</i-grid-label>
+    </i-grid-item>
+    <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/drink.png" />
+        </i-grid-icon>
+        <i-grid-label>饮料</i-grid-label>
+    </i-grid-item>
+    <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/ice cream.png" />
+        </i-grid-icon>
+        <i-grid-label>甜品</i-grid-label>
+    </i-grid-item>
+  </i-grid>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  },
+
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+</style>
