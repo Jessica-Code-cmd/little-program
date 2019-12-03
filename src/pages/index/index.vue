1996379dd6559e0b347c1f3684f6195fd764b60e
Jessica <529933143@qq.com>
2019-12-03 15:13:44 +0800

    update kk4

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,24 +1,35 @@
 <template>
   <div>
+    <swiper
+      :indicator-dots="indicatorDots"
+      :autoplay="autoplay"
+      :interval="interval"
+      :duration="duration"
+    >
+    <block v-for="img in imgUrls" :key="img">
+      <swiper-item>
+        <image :src="img" style="width:100%;"/>
+      </swiper-item>
+    </block>
+    </swiper>
     <i-grid i-class="no-border">
-    <i-grid-item @click="goType(grid)" v-for="grid in grids" :key="grid" i-class="no-border">
-        <i-grid-icon>
-            <image :src="grid.image" />
-        </i-grid-icon>
-        <i-grid-label>{{grid.title}}</i-grid-label>
-    </i-grid-item>
-  </i-grid>
-  <i-panel :title="title_name">
+      <i-grid-item @click="goType(grid)" v-for="grid in grids" :key="grid" i-class="no-border">
+          <i-grid-icon>
+              <image :src="grid.image" />
+          </i-grid-icon>
+          <i-grid-label>{{grid.title}}</i-grid-label>
+      </i-grid-item>
+    </i-grid>
+    <i-panel :title="title_name">
     <view style="padding: 15px;">
-    <i-card v-for="item in top" :key="item" i-class="split" :extra="item.name" :thumb="item.image">
+      <i-card v-for="item in top" :key="item" i-class="split" :extra="item.name" :thumb="item.image">
         <view slot="content">{{item.remark}}</view>
         <view slot="footer">{{item.address}}</view>
-</i-card>
+      </i-card>
     </view>
-</i-panel>
+  </i-panel>
   </div>
 </template>
-
 <script>
 
 export default {
@@ -36,13 +47,24 @@ export default {
         {name:"店名2",address:"地址2",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍2"},
         {name:"店名3",address:"地址3",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍3"},
         {name:"店名4",address:"地址4",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍4"}
-      ]
+      ],
+      imgUrls:[
+          'http://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'http://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+          'http://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+   ],
+      indicatorDots: false,
+      autoplay: false,
+      interval: 5000,
+      duration: 1000
     }
   },
 
-  methods: {
+   methods: {
     goType(type){
       console.log(type)
+      let url = '../list/main?type=' + type.title
+      mpvue.navigateTo({ url })
     }
   },
 
